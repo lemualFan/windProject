40d1b071ac88fd28b2b0eeee51235205ca4726a1
flm <lemualfan@163.com>
2018-12-24 10:27:01 +0800

    time_interval_test

--- time_interval_test.py
+++ time_interval_test.py
@@ -1,29 +1,3 @@
-from  data import loaddata
-from config import RAW_DATA,PROCESS_LEVEL1
-import pandas as pd
-from series_to_supervised_learning import series_to_supervised
-data = loaddata.load_data(RAW_DATA)
-data.to_csv(PROCESS_LEVEL1)
-
-from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import MinMaxScaler
-
-
-
-from keras import Sequential
-from keras.layers import LSTM, Dense
-import matplotlib.pyplot as plt
-from numpy import concatenate  # 数组拼接
-from math import sqrt
-from sklearn.metrics import mean_squared_error
-
-
-ndata = data[['time','power_30s_avr','speed_wind_30s_avr']]
-values = ndata.iloc[:,1:3].values
-scaler = MinMaxScaler(feature_range=(0, 1))
-scaled = scaler.fit_transform(values)
-scaled_columns = ['power_30s_avr', 'speed_wind_30s_avr']
-
 # 将序列数据转化为监督学习数据
 """
     Frame a time series as a supervised learning dataset.
@@ -35,22 +9,18 @@ scaled_columns = ['power_30s_avr', 'speed_wind_30s_avr']
     Returns:
         Pandas DataFrame of series framed for supervised learning.
 """
-n_in = 100
+n_in = 50
 #调整n_in 即可
-
-# 将序列数据转化为监督学习数据
-reframed = series_to_supervised(scaled, scaled_columns, n_in, 1)
-# 只将power作为y
+reframed = series_to_supervised(scaled, scaled_columns,n_in, 1)
+# 只将power作为y,去除speed，形如['power_30s_avr1(t-5)', 'speed_wind_30s_avr2(t-5)',
+#        'power_30s_avr1(t-4)', 'speed_wind_30s_avr2(t-4)',
+#        'power_30s_avr1(t-3)', 'speed_wind_30s_avr2(t-3)',
+#        'power_30s_avr1(t-2)', 'speed_wind_30s_avr2(t-2)',
+#        'power_30s_avr1(t-1)', 'speed_wind_30s_avr2(t-1)', 'power_30s_avr1(t)']
 reframed.drop(reframed.columns[[reframed.shape[1]-1]], axis=1, inplace=True)
-
-#只保留t-n_in的数据
-drop_columns = range(2,n_in*2)
-reframed.drop(reframed.columns[drop_columns], axis=1, inplace=True)
-
 values = reframed.values
 
-
-n_trian_hours = 24*120*25
+n_trian_hours = 24*120*15
 train = values[:n_trian_hours,:]
 test = values[n_trian_hours:,:]
 train_x, train_y = train[:, :-1], train[:, -1]
@@ -59,6 +29,7 @@ test_x, test_y = test[:, :-1], test[:, -1]
 # 为了在LSTM中应用该数据，需要将其格式转化为3D format，即[Samples, timesteps, features]
 train_X = train_x.reshape((train_x.shape[0],1, train_x.shape[1]))
 test_X = test_x.reshape(( test_x.shape[0],1, test_x.shape[1]))
+
 
 model = Sequential()
 model.add(LSTM(10, input_shape=(train_X.shape[1], train_X.shape[2])))
@@ -96,8 +67,5 @@ ahead_second = n_in * 30
 
 plt.figure(21)
 plt.suptitle("%s s ahead,Test RMSE:%s"%(ahead_second,rmse))
-plt.subplot(211),plt.plot(inv_yHat,label='predict')
-plt.legend()
-plt.subplot(212),plt.plot(inv_y,label='raw')
-plt.legend()
-plt.show()
+plt.subplot(211),plt.plot(inv_yHat)
+plt.subplot(212),plt.plot(inv_y)
